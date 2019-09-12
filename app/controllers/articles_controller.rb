cf7c997c4136ea584e056e317fa1d41a296e5a8b
Jayanta Gogoi <codergogoi@gmail.com>
2019-09-12 08:31:08 +0530

    Added Show View

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,6 @@
 class ArticlesController < ApplicationController
 
   def new
-
     @article = Article.new
   end
 
@@ -9,8 +8,17 @@ class ArticlesController < ApplicationController
       # Inspect post data to the server
       # render plain: params[:article].inspect
     @article = Article.new(article_params)
-    @article.save
-    redirect_to article_show(@article)
+     if @article.save
+       flash[:notice] = "Article was successfully created!"
+       redirect_to article_path(@article)
+     else
+       render 'new'
+     end
+
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 
   private

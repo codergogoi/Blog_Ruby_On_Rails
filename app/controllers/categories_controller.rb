80754172f1874c2002195c17462612db30197961
Jayanta Gogoi <codergogoi@gmail.com>
2019-09-22 19:12:21 +0530

    Added Category Edit features for Admin

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,7 @@
 class CategoriesController < ApplicationController
 
+  before_action :require_admin, except: [:index, :show]
+
   def index
     @categories = Category.paginate(page: params[:page], per_page: 5)
   end
@@ -18,7 +20,27 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:success] = "Category name was updated"
+      redirect_to category_path(@category)
+    else
+      render 'edit'
+    end
+
+
+  end
+
+
+
   def show
+    @category = Category.find(params[:id])
+    @category_articles  = @category.articles.paginate(page: params[:page], per_page: 5)
 
   end
 
@@ -26,6 +48,13 @@ class CategoriesController < ApplicationController
 
   def category_params
       params.require(:category).permit(:name)
+  end
+
+  def require_admin
+    if !logged_in? || (logged_in? and !current_user.admin?)
+      flash[:danger] = "Only Admin User can perform this"
+      redirect_to categories_path
+    end
   end
 
 end
